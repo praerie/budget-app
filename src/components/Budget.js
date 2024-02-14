548d6b07c996a68e3a19ef8015cf4c828461eb8b
Adelie <elisabethadelie@gmail.com>
2024-02-14 18:10:12 -0500

    render components

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,27 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget, setBudget } = useContext(AppContext); 
+    const [newBudget, setNewBudget] = useState(budget);
+
+    const handleBudgetChange = (event) => {
+        const newBudgetValue = parseFloat(event.target.value);
+        setNewBudget(newBudgetValue); // update local state
+        setBudget(newBudgetValue); // update context value
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input
+                type="number"
+                step="10"
+                value={newBudget}
+                onChange={handleBudgetChange}
+            />
+        </div>
+    );
+};
+
+export default Budget;
